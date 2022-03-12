8bc204acea045a9e8590852b79e650a53aa020fd
hugosenari <hugosenari@gmail.com>
2022-03-12 02:33:17 -0300

    chore: re index daemon

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Cross-platform process daemonization library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-daemon-master.flake = false;
+  inputs.src-daemon-master.owner = "status-im";
+  inputs.src-daemon-master.ref   = "refs/heads/master";
+  inputs.src-daemon-master.repo  = "nim-daemon";
+  inputs.src-daemon-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-daemon-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-daemon-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
